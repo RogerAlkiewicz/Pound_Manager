7eb9af161d3eb9bad19130dd92fbbe2d80d26837
Roger Alkiewicz <alkiewiczroger@gmail.com>
2021-08-16 19:42:28 +0200

    Commit no.2 *message starts* Changes: - solved issue with deleting records from the array. - changed add algorithm to search for free space in loop in the array. - improved status case to show appropriate message depending on how many   animals are currently recorded. Problems: - none so far Plans: - implement classes and functions - add security logic (exceptions) for user input *message ends*

--- Pound_01/Pound_01/Program.cs
+++ Pound_01/Pound_01/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 /*
  * Program pozwala na dodawanie/usuwanie z bazy zwierząt oraz sprawdzanie stanu schroniska (puste, umiarkowane, przepełnione)
- * Wariat I - bardzo prosty:
+ * Wariat 1 - bardzo prosty:
  * - schronisko ma mieć określoną liczbę miejsc
  * - możliwość dodania/usunięcia zwierzaka do/z listy w schronisku (bez bazy danych, czy pliku tekstowego)
  * - po wpisaniu "status" wyświetli aktualną liczbę zwierząt i komunikat, co oznacza ta liczba zwierząt (puste, umiarkowane, przepełnione)
@@ -16,47 +16,64 @@ class Program
         Console.WriteLine("Type \"add\" to add a new animal, or \"del\" to delete it from the database.");
         Console.WriteLine("To show pound's current status type \"status\". To exit the application enter \"exit\".");
         string[] pound_space = new string[5];
-        int room = 0;
+        int space = 0;
         bool leave = false;
         string read = string.Empty;
         while (leave != true)
         {
             read = Console.ReadLine();
-            switch (read)
+            switch (read)            
             {
                 case "add":
                     {
                         Console.WriteLine("What do you want to add?");
                         read = Console.ReadLine();
-                        pound_space[room] = read;
-                        Console.WriteLine($"You have added {read} to the pound's database.");
-                        room++;
+                        // loop searches for the first free slot in the array
+                        // if there ain't any, the if condition won't allow to put a new record in the array
+                        for (int i = 0; i < pound_space.Length; i++)
+                        {
+                            if (pound_space[i] == null)
+                            {
+                                pound_space[i] = read;
+                                Console.WriteLine($"You have added {read} to the pound's database.");
+                                space++;
+                                break;
+                            }
+                            else if (i == pound_space.Length - 1)
+                                Console.WriteLine("Pound's capacity is full.");
+                        }
                         break;
                     }
                 case "del":
                     {
                         Console.WriteLine("What do you want to delete?");
                         read = Console.ReadLine();
+                        // loop searches for the matching string in the array
+                        // if keyword don't match at the last iteration the else if condition outputs an appropriate information
                         for (int i = 0; i < pound_space.Length; i++)
                         {
                             if (pound_space[i] == read)
                             {
                                 pound_space[i] = string.Empty;
                                 Console.WriteLine($"You have deleted {read} from the pound's database.");
-                                room--;
+                                space--;
                                 break;
                             }
-                            else
-                            {
-                                Console.WriteLine($"There isn't any {read} in the database. ");
-                                break;
-                            }
+                            else if (i == pound_space.Length - 1)
+                                Console.WriteLine($"There is not any {read} in the database.");
                         }
+
                         break;
                     }
                 case "status":
                     {
-                        Console.WriteLine($"Currently there are {room} animals in the pound's database."/*dodaj info co oznacza ta liczba*/);
+                        // based on "space" variable's value algorithm displays wheather the pound is full, empty
+                        if (space == pound_space.Length)
+                            Console.WriteLine($"Currently there are {space} animals. Pound is full and cannot take in more animals.");
+                        else if (space > 0 || space < pound_space.Length)
+                            Console.WriteLine($"Currently there are {space} animals in the pound's database.");
+                        else
+                            Console.WriteLine("Curretnly the pound is empty.");
                         break;
                     }
                 case "exit":
