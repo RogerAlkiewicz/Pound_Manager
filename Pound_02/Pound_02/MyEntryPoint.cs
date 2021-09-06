51c610f476219e4d7ebd2df0f6df3f95a4c26864
Roger Alkiewicz <alkiewiczroger@gmail.com>
2021-09-06 19:44:37 +0200

    ui-branch commit no.2.1 *message starts* Changes: - changed application's entry point to custom Main method How? - create new class (on new code page),   implement Main function there, then   go to: Project -> Edit Project File   add new startup point by writing:   <Startup> Namespace.ClassName </StartupObject> (location of custom Main method) Problems: - understanding flow of WPF applications; where does it start and how to change the starting point Next session: - implement algorithm which loads text file or creates new one if it dosen't exist

---
+++ Pound_02/Pound_02/MyEntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pound_02
+{
+    class MyEntryPoint
+    {
+        [STAThread]
+        static void Main()
+        {
+            App application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
